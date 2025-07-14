022bb2d7919ed80e8d9b87bfa8fb2c1343e33a24
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-07-14 09:21:24 +0530

    implemented file handling

--- app/main.py
+++ app/main.py
@@ -1,21 +1,58 @@
+import io
 import pathlib
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import os
+import uuid
+from functools import lru_cache
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    Depends,
+    Request,
+    File,
+    UploadFile,)
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from pydantic_settings import BaseSettings
 
+class Settings(BaseSettings):
+    debug: bool = False
+    echo_active: bool = False
 
-# FastAPI always returns a JSON response by default
-# To return HTML, we need to specify the response class
+    class Config:
+        env_file = ".env"
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+settings = get_settings()
+DEBUG = settings.debug
+
 
 BASE_DIR = pathlib.Path(__file__).parent
-print( BASE_DIR/ "templates" )
+UPLOAD_DIR = BASE_DIR / "uploads"
+
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
+# FastAPI always returns a JSON response by default
+# To return HTML, we need to specify the response class
 @app.get("/", response_class=HTMLResponse)
-def home_view(request: Request):
-    return templates.TemplateResponse({"request": request}, "home.html",{ "developer": "MadhushankhaDeS"})
+def home_view(request: Request, settings:Settings = Depends(get_settings)):
+    return templates.TemplateResponse({"request": request}, "home.html",{ "developer": "MadhushankhaDeS", })
 
 @app.post("/")
 def home_detail_view():
     return {"message": "Welcome to the FastAPI application!"}
+
+@app.post("/img-echo/", response_class=FileResponse)
+async def img_echo_view(file:UploadFile = File(...), settings:Settings = Depends(get_settings)):
+    if not settings.echo_active:
+        raise HTTPException(status_code=400, detail="Echo is not active")
+    bytes_str = io.BytesIO(await file.read())
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}" # To avoid filename conflicts
+    with open(str(dest), "wb") as out:
+        out.write(bytes_str.read())
+    return dest
